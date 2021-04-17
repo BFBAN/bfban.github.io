1c153356a61cd8c0a044f5ef4047172da87b25fd
Duck <35273601+nicoth-in@users.noreply.github.com>
2021-04-17 23:30:43 +0300

    Add Russian lang to the selector in the footer

--- front/src/views/Footer.vue
+++ front/src/views/Footer.vue
@@ -31,6 +31,7 @@ export default {
         {value: 'zh-CN', label: '简体中文'},
         {value: 'en-US', label: 'English'},
         {value: 'ja-JP', label: '日本語'},
+        {value: 'ru-RU', label: 'Русский'},
       ]
     }
   },
